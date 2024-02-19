01a8263d363e718cc5b90b1acaac75203b041bfe
Ron Zilbershtein <zron13il@gmail.com>
2024-02-19 12:50:07 +0200

    Add pingServer and time measurements functions

--- LocalServer/Client/client.go
+++ LocalServer/Client/client.go
@@ -7,6 +7,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -29,54 +32,71 @@ func main() {
 		log.Fatal(err)
 	}
 	defer conn.Close()
-
 	for {
 		if !sendMessage(conn) {
 			break
 		}
 		handleRequest(conn)
 	}
-
 }
 
 func sendMessage(conn net.Conn) (status bool) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Text to send: ")
-	text, _ := reader.ReadString('\n')
-	if text == "exit\n" {
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+		return true // Continue connection
+	}
+	switch strings.TrimSpace(text) {
+	case "exit":
 		fmt.Println(conn.LocalAddr().String(), "Disconnecting")
 		fmt.Fprintf(conn, text+"\n")
-		return false
+		return false // Close connection
 
-	} else if text == "lorem\n" {
+	case "lorem":
 		sendFile(conn, "lorem ipsum.txt")
 		return true
+
+	case "time":
+		timeStamp := time.Now().UnixMicro()
+		fmt.Fprintf(conn, "Time: "+strconv.Itoa(int(timeStamp))+"\n")
+
+	case "ping":
+		pingServer(conn)
+	default:
+		fmt.Fprintf(conn, text+"\n")
 	}
-	// Send the text to the server:
-	fmt.Fprintf(conn, text+"\n")
 	return true
 }
 
 func handleRequest(conn net.Conn) {
-	message, _ := bufio.NewReader(conn).ReadString('\n')
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	checkError(err)
 	fmt.Print("Message from the server: " + message)
+
+	if len(message) >= len("TIME") && message[:len("TIME")] == "TIME" {
+		parts := strings.Fields(message)	
+		oldTimeStamp, err := strconv.Atoi(parts[1])
+		checkError(err)
+		newTimeStamp := time.Now().UnixMicro()
+		elapsedTime := newTimeStamp - int64(oldTimeStamp)
+		fmt.Println("Elapsed time:", elapsedTime, "Microseconds")
+	}
 }
 
 func sendFile(conn net.Conn, fileName string) {
 	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	// close file on exit and check for its returned error
 	defer func() {
 		if err := file.Close(); err != nil {
 			log.Fatal(err)
 		}
 	}()
-
 	// make a buffer to keep chunks that are read
 	reader := bufio.NewReader(file)
-
 	for {
 		// read a chunk
 		s, err := reader.ReadString('\n')
@@ -90,5 +110,24 @@ func sendFile(conn net.Conn, fileName string) {
 		fmt.Fprintf(conn, s+"\n")
 		break
 	}
+}
 
+// checkError General error handling
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pingServer(conn net.Conn) {
+	i := 1
+	count := 100
+	frequency := 100 * time.Millisecond
+	for i < count {
+		fmt.Fprintf(conn, "Ping "+strconv.Itoa(i)+"\n")
+		handleRequest(conn)
+		time.Sleep(frequency)
+		i++
+	}
+	fmt.Fprintf(conn, "Ping "+strconv.Itoa(i)+"\n") // Send the last ping
 }
